d60be4369eb44eb57efeff6ca4774f699d8f422c
mVolegov <volegovmikhail12@gmail.com>
2023-02-03 21:05:25 +0500

    Add API versioning

--- src/main/java/com/mvoleg/coffeespringbootapp/restcontroller/MenuElementRestController.java
+++ src/main/java/com/mvoleg/coffeespringbootapp/restcontroller/MenuElementRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/menu-elements")
+@RequestMapping("/api/v1/menu-elements")
 public class MenuElementRestController {
 
     private final MenuElementService menuElementService;
--- src/main/java/com/mvoleg/coffeespringbootapp/restcontroller/OrderRestController.java
+++ src/main/java/com/mvoleg/coffeespringbootapp/restcontroller/OrderRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api/v1/orders")
 public class OrderRestController {
 
     private final OrderService orderService;
